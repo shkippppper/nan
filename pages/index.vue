9c198c63f96af55e7cf547e8a88df209f65f0fb0
shkippppper <a.titberidze@yahoo.com>
2023-12-08 11:58:47 +0400

    changed index file to match new design

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template lang="pug">
-  tutorial
+.wrapper
+    NavigationListing
 </template>
 
 <script>
 import Vue from 'vue'
+import NavigationListing from '../components/NavigationListing.vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: 'IndexPage',
+    components: {
+        NavigationListing
+    }
 })
 </script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'arial', sans-serif;
+
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 10px;
+    line-height: 20px;
+    font-weight: 200;
+}
+</style>
