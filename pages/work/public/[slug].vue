5174921b8d6cf6abcb1148170de8a41885ef9802
shkippppper <a.titberidze@yahoo.com>
2023-12-08 12:00:19 +0400

    added 2 new components for testing purposes

---
+++ pages/work/public/[slug].vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.asd aaa
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+
+</style>
---
+++ pages/work/work.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.asd {aaaa
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+
+</style>
