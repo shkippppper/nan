ec9a62d5a72e368aff67889828921253b7b020d4
shkippppper <a.titberidze@yahoo.com>
2023-12-10 23:26:33 +0400

    created a default vue state

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+div
+    NavigationListing
+    Nuxt
+</template>
+
+<script>
+import NavigationListing from "./NavigationListing.vue";
+export default {
+    components: {
+        NavigationListing,
+    }
+}
+</script>
+
+
+<style>
+
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'arial', sans-serif;
+
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 10px;
+    line-height: 20px;
+    font-weight: 200;
+}
+
+</style>
