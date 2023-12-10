75be4de4eb18972668a3898ec53babbb74fb2e1a
shkippppper <a.titberidze@yahoo.com>
2023-12-10 23:28:15 +0400

    removed extra code (might need to delete)

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,12 @@
 <template lang="pug">
 .wrapper
-    NavigationListing
+
 </template>
 
 <script>
-import Vue from 'vue'
-import NavigationListing from '../components/NavigationListing.vue'
-
-export default Vue.extend({
+export default {
     name: 'IndexPage',
-    components: {
-        NavigationListing
-    }
-})
-</script>
 
-<style>
-*{
-    margin: 0;
-    padding: 0;
-    font-family: 'arial', sans-serif;
-
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    font-size: 10px;
-    line-height: 20px;
-    font-weight: 200;
 }
-</style>
+
+</script>
